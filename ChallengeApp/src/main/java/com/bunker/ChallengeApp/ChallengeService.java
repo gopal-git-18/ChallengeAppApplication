6016b84ac916cf23abfac3cecb9bd5eab9487220
gopal-git-18 <gopalshete057@gmail.com>
2025-10-19 10:13:43 +0530

    initial commit

---
+++ ChallengeApp/src/main/java/com/bunker/ChallengeApp/ChallengeService.java
@@ -0,0 +1,63 @@
+package com.bunker.ChallengeApp;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ChallengeService {
+    private List<Challenge> challenges =
+            new ArrayList<>();
+    private Long nextId = 1L;
+
+    public ChallengeService(){
+
+    }
+
+
+
+    public List<Challenge> getAllChallenges(){
+        return challenges;
+    }
+
+    public boolean addChallenge( Challenge challenge){
+        if(challenge != null){
+            challenge.setId(nextId++);
+            challenges.add(challenge);
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+//    returns only one challenge for matching month
+    public Challenge getChallenges(String month) {
+        for(Challenge challenge: challenges){
+            if(challenge.getMonth().equalsIgnoreCase(month)){
+                return challenge;
+            }
+        }
+        return null;
+    }
+
+
+    public boolean updateChallenge(Long id, Challenge updatedChallenge) {
+        for(Challenge challenge: challenges){
+            if(challenge.getId().equals(id)){
+                challenge.setMonth(updatedChallenge.getMonth());
+                challenge.setDescription(updatedChallenge.getDescription());
+                return true;
+            }
+
+            }
+        return false;
+    }
+
+
+    public boolean deleteChallenge(Long id) {
+        return challenges.removeIf(challenge -> challenge.getId().equals(id));
+    }
+}
